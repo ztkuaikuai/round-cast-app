36f3011107ee7277539cd08ba448453f4c2ea7ba
zhangtao77 <zhangtao77@baidu.com>
2025-08-28 14:16:45 +0800

    perf(router): 删除顶部header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
-import './global.css';
+import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack />
+    <Stack 
+      screenOptions={{
+        headerShown: false
+      }}
+    />
   );
 }
--- app/task/[taskId]/index.tsx
+++ app/task/[taskId]/index.tsx
@@ -1,11 +1,12 @@
 import { Link, useLocalSearchParams } from 'expo-router'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Task = () => {
     const { taskId } = useLocalSearchParams()
+    console.log("🚀 ~ Task ~ taskId:", taskId)
     return (
         <View>
-            <View>taskId: {taskId}</View>
+            <Text>taskId: {taskId}</Text>
             <Link href="/home">Go to home screen</Link>
             <Link href="/sidebar">Go to sidebar screen</Link>
             <Link href="/user-info">Go to user info screen</Link>
